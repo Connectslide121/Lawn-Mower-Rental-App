4e3fa30eaeccda81869456832beeded49cb69ee2
Jon Mendizabal <jonmendi@gmail.com>
2023-10-16 15:59:59 +0200

    Add Main Menu

---
+++ Lawn Mower Rental App/Controller/MainMenuController.cs
@@ -0,0 +1,44 @@
+﻿using Lawn_Mower_Rental_App.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lawn_Mower_Rental_App.Controller
+{
+    public class MainMenuController
+    {
+        public static void MenuController(string userInput)
+        {
+                if (int.TryParse(userInput, out int userInputInt))
+                {
+                    switch (userInputInt)
+                    {
+                        case 1:
+                            Console.WriteLine("Going to RentLawnMower..."); //REPLACE WITH A CALL FOR THE METHOD
+                            break;
+                        case 2:
+                            Console.WriteLine("Going to ReturnLawnMower..."); //REPLACE WITH A CALL FOR THE METHOD
+                            break;
+                        case 3:
+                            Console.WriteLine("Going to CurrentInventory..."); //REPLACE WITH A CALL FOR THE METHOD
+                            break;
+                        case 4:
+                            Console.WriteLine("Going to NewCustomer..."); //REPLACE WITH A CALL FOR THE METHOD
+                            break;
+                        case 5:
+                            Console.WriteLine("Going to CustomerList..."); //REPLACE WITH A CALL FOR THE METHOD
+                            break;
+                        case 6:
+                            Console.WriteLine("Exiting the app...");
+                            return;
+                    }
+                }
+                else 
+                { 
+                    Main.MainMenu();                
+                }
+        }
+    }
+}
---
+++ Lawn Mower Rental App/View/Main.cs
@@ -0,0 +1,47 @@
+﻿using Lawn_Mower_Rental_App.Controller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lawn_Mower_Rental_App.View
+{
+    public class Main
+    {
+        public static void MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("|***************************************** LAWN MOWER RENTAL (TM) **************************************|");
+            Console.WriteLine("|\t\t\t\t\t\t\t\t\t\t\t\t\t|");
+            Console.WriteLine("|\t\t\t\t\t\t\t\t\t\t\t\t\t|");
+            Console.WriteLine("|\t\t\t\t\t\t\t\t\t\t\t\t\t|");
+            Console.WriteLine("|\t\t\t\t\t\t MAIN MENU \t\t\t\t\t\t|");
+            Console.WriteLine("|\t\t\t\t\t\t\t\t\t\t\t\t\t|");
+            Console.WriteLine("|\t\t\t Select an option by entering the corresponding number: \t\t\t|");
+            Console.WriteLine("|\t\t\t\t\t\t\t\t\t\t\t\t\t|");
+            Console.WriteLine("|\t\t\t\t\t\t\t\t\t\t\t\t\t|");
+            Console.WriteLine("|\t\t\t\t\t 1.- Rent a lawn mower \t\t\t\t\t\t|");
+            Console.WriteLine("|\t\t\t\t\t\t\t\t\t\t\t\t\t|");
+            Console.WriteLine("|\t\t\t\t\t 2.- Return a lawn mower \t\t\t\t\t|");
+            Console.WriteLine("|\t\t\t\t\t\t\t\t\t\t\t\t\t|");
+            Console.WriteLine("|\t\t\t\t\t 3.- View current inventory \t\t\t\t\t|");
+            Console.WriteLine("|\t\t\t\t\t\t\t\t\t\t\t\t\t|");
+            Console.WriteLine("|\t\t\t\t\t\t\t\t\t\t\t\t\t|");
+            Console.WriteLine("|\t\t\t\t\t 4.- Register new customer \t\t\t\t\t|");
+            Console.WriteLine("|\t\t\t\t\t\t\t\t\t\t\t\t\t|");
+            Console.WriteLine("|\t\t\t\t\t 5.- View list of customers \t\t\t\t\t|");
+            Console.WriteLine("|\t\t\t\t\t\t\t\t\t\t\t\t\t|");
+            Console.WriteLine("|\t\t\t\t\t\t\t\t\t\t\t\t\t|");
+            Console.WriteLine("|\t\t\t\t\t 6.- Exit \t\t\t\t\t\t\t|");
+            Console.WriteLine("|\t\t\t\t\t\t\t\t\t\t\t\t\t|");
+            Console.WriteLine("|\t\t\t\t\t\t\t\t\t\t\t\t\t|");
+            Console.WriteLine("|\t\t\t\t\t\t\t\t\t\t\t\t\t|");
+            Console.WriteLine("|*******************************************************************************************************|");
+            Console.WriteLine();
+            Console.WriteLine("Enter the number of your choice:");
+
+            MainMenuController.MenuController(Console.ReadLine());
+        }
+    }
+}
