54149c4dc05f2a391515a7af96dfeaa1d8bf2663
DanarchyLarusson <larussondaniel@gmail.com>
2023-10-16 14:00:54 +0200

    Orginazation test

--- Lawn Mower Rental App/Program.cs
+++ Lawn Mower Rental App/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("This is an test commit.");
+Console.WriteLine("49384 is a weird number");
