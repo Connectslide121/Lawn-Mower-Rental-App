6dbc7c4afc908aaa597a6732a9b1efc074a90fce
DanarchyLarusson <larussondaniel@gmail.com>
2023-10-16 16:35:13 +0200

    Customer and Customer Manager Class made.

---
+++ Lawn Mower Rental App/Controller/CustomerManager.cs
@@ -0,0 +1,54 @@
+﻿using Lawn_Mower_Rental_App.Model;
+using System.Text.Json;
+using System.Xml;
+
+public class CustomerManager
+{
+    private List<Customer> customers;
+
+    public CustomerManager()
+    {
+        customers = LoadCustomersFromJson();
+    }
+    // Just setting up so we can interact with the json file.
+    public void RegisterNewCustomer(Customer customer)
+    {
+        customers.Add(customer);
+        SaveCustomersToJson();
+        Console.WriteLine("Customer registered successfully.");
+    }
+
+    public void ViewListOfCustomers()
+    {
+        if (customers.Count == 0)
+        {
+            Console.WriteLine("No customers registered yet.");
+            return;
+        }
+
+        Console.WriteLine("List of registered customers:");
+        foreach (var customer in customers)
+        {
+            Console.WriteLine(customer);
+        }
+    }
+
+    private List<Customer> LoadCustomersFromJson()
+    {
+        if (File.Exists("CustomerData.json"))
+        {
+            string jsonData = File.ReadAllText("CustomerData.json");
+            return JsonSerializer.Deserialize<List<Customer>>(jsonData);
+        }
+        return new List<Customer>();
+    }
+    private void SaveCustomersToJson()
+    {
+        string jsonData = JsonSerializer.Serialize(customers, typeof(List<Customer>), new JsonSerializerOptions
+        {
+            WriteIndented = true
+        });
+        File.WriteAllText("CustomerData.json", jsonData);
+    }
+
+}
---
+++ Lawn Mower Rental App/Model/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lawn_Mower_Rental_App.Model
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int ContactNumber { get; set; }
+        public string Address { get; set; }
+        // We can add more if we need it
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} - Contact: {ContactNumber}, Address: {Address}";
+        }
+    }
+
+}
