0116e5bd8ad25d1b645ffda39f5587190f37a508
Jon Mendizabal <jonmendi@gmail.com>
2023-10-25 11:53:55 +0200

    Divide MainMenu in submenus

--- Lawn Mower Rental App/Controller/MainMenuController.cs
+++ Lawn Mower Rental App/Controller/MainMenuController.cs
@@ -12,7 +12,43 @@ namespace Lawn_Mower_Rental_App.Controller
     public class MainMenuController
     {
         
-        public static void MenuController(string userInput)
+        public static void MainMenuController_(string userInput)
+        {
+            if (int.TryParse(userInput, out int userInputInt))
+            {
+                switch (userInputInt)
+                {
+
+                    case 1:
+                        MainMenu.RentalMenu();
+                        break;
+                    case 2:
+                        MainMenu.LawnMowerMenu();
+                        break;
+                    case 3:
+                        MainMenu.CustomerMenu();
+                        break;
+                    case 4:
+                        MainMenu.AccountingMenu();
+                        break;
+
+                    case 5:
+                        Console.WriteLine("Exiting the app...");
+                        Environment.Exit(0);
+                        break;
+
+                    default: 
+                        MainMenu.MainMenu_();
+                        break;
+                }
+            }
+            else 
+            { 
+                MainMenu.MainMenu_();
+            }
+        }
+
+        public static void RentalMenuController(string userInput)
         {
             RentLawnMowerForm rentLawnMowerForm = new RentLawnMowerForm();
             ReturnLawnMowerForm returnLawnMowerForm = new ReturnLawnMowerForm();
@@ -20,96 +56,115 @@ namespace Lawn_Mower_Rental_App.Controller
             ViewActiveRentalsForm viewActiveRentalsForm = new ViewActiveRentalsForm();
             ViewRentalHistoryForm viewRentalHistoryForm = new ViewRentalHistoryForm();
 
+            if (int.TryParse(userInput, out int userInputInt))
+            {
+                switch (userInputInt)
+                {
+                    case 1:
+                        rentLawnMowerForm.RentLawnMowerForm_();
+                        break;
+                    case 2:
+                        returnLawnMowerForm.ReturnLawnMowerForm_();
+                        break;
+                    case 3:
+                        rentalManager.ViewReturnsToday();
+                        break;
+                    case 4:
+                        rentalManager.GetDelayedRentals();
+                        break;
+                    case 5:
+                        viewActiveRentalsForm.ViewActiveRentalsForm_();
+                        break;
+                    case 6:
+                        viewRentalHistoryForm.ViewRentalHistoryForm_();
+                        break;
+                    default:
+                        MainMenu.RentalMenu();
+                        break;
+
+                }
+            }
+        }
+        public static void LawnMowerMenuController(string userInput)
+        {
             NewLawnMowerForm newLawnMowerForm = new NewLawnMowerForm();
             DeleteLawnMowerForm deleteLawnMowerForm = new DeleteLawnMowerForm();
             LawnMowerManager lawnMowerManager = new LawnMowerManager();
             UpdateMaintenanceForm updateMaintenanceForm = new UpdateMaintenanceForm();
 
+            if (int.TryParse(userInput, out int userInputInt))
+            {
+                switch (userInputInt)
+                {
+                    case 1:
+                        newLawnMowerForm.NewLawnMowerForm_();
+                        break;
+                    case 2:
+                        deleteLawnMowerForm.DeleteLawnMowerForm_();
+                        break;
+                    case 3:
+                        lawnMowerManager.ViewListOfLawnMowers();
+                        break;
+                    case 4:
+                        updateMaintenanceForm.UpdateMaintenanceForm_();
+                        break;
+                    default:
+                        MainMenu.LawnMowerMenu();
+                        break;
+
+                }
+            }
+
+        }
+
+        public static void CustomerMenuController(string userInput)
+        {
             NewCustomerForm newCustomerForm = new NewCustomerForm();
             DeleteCustomerForm deleteCustomerForm = new DeleteCustomerForm();
             CustomerManager customerManager = new CustomerManager();
             CustomerSearchView customerSearchView = new CustomerSearchView();
 
+            if (int.TryParse(userInput, out int userInputInt))
+            {
+                switch (userInputInt)
+                {
+                    case 1:
+                        newCustomerForm.NewCustomerForm_();
+                        break;
+                    case 2:
+                        deleteCustomerForm.DeleteCustomerForm_();
+                        break;
+                    case 3:
+                        customerManager.ViewListOfCustomers();
+                        break;
+                    case 4:
+                        customerSearchView.SearchCustomers();
+                        break;
+                    default:
+                        MainMenu.CustomerMenu();
+                        break;
+
+                }
+            }
+
+        }
+
+        public static void AccountingMenuController(string userInput)
+        {
             EarningsProfitView earningsProfitView = new EarningsProfitView();
 
-            
-
             if (int.TryParse(userInput, out int userInputInt))
+            {
+                switch (userInputInt)
                 {
-                        switch (userInputInt)
-                        {
+                    case 1:
+                        earningsProfitView.DisplayEarningsProfitMenu();
+                        break;
+                    default:
+                        MainMenu.AccountingMenu();
+                        break;
 
-                        case 1:
-                            rentLawnMowerForm.RentLawnMowerForm_();
-                            break;
-                        case 2:
-                            returnLawnMowerForm.ReturnLawnMowerForm_();
-                            break;
-                        case 3:
-                            rentalManager.ViewReturnsToday();
-                            break;
-                        case 4:
-                            rentalManager.GetDelayedRentals();
-                            break;
-                        case 5:
-                            viewActiveRentalsForm.ViewActiveRentalsForm_();
-                            break;
-                        case 6:
-                            viewRentalHistoryForm.ViewRentalHistoryForm_();
-                            break;
-
-
-                        case 7:
-                            newLawnMowerForm.NewLawnMowerForm_();
-                            break;
-                        case 8:
-                            deleteLawnMowerForm.DeleteLawnMowerForm_();
-                            break;
-                        case 9:
-                            lawnMowerManager.ViewListOfLawnMowers();
-                            break;
-                        case 10:
-                            updateMaintenanceForm.UpdateMaintenanceForm_();
-                            break;
-
-
-                        case 11:
-                            newCustomerForm.NewCustomerForm_();
-                            break;
-                        case 12:
-                            deleteCustomerForm.DeleteCustomerForm_();
-                            break;
-                        case 13:
-                            customerManager.ViewListOfCustomers();
-                            break;
-                        case 14:
-                            customerSearchView.SearchCustomers();
-                             break;
-
-
-
-
-                        case 15:
-                            earningsProfitView.DisplayEarningsProfitMenu();
-                            break;
-
-
-                        case 16:
-                            Console.WriteLine("Exiting the app...");
-                            Environment.Exit(0);
-                            break;
-
-
-                        default: 
-                            MainMenu.MainMenu_();
-                            break;
-                    }
-
-            }
-            else 
-            { 
-                MainMenu.MainMenu_();
-
+                }
             }
         }
     }
